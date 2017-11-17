4b18b269ccd2bf21c64f8cf2ddefcb6656fcffc1
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-17 21:20:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 function happyHolidays() {
-return("Happy holidays!");
+return"Happy holidays!";
 }
 function happyHolidaysTo(you) {
-return("Happy holidays, you!");
+return"Happy holidays, you!";
 }
 function happyHolidayTo(holiday, name)
 return`Happy ${holiday}, ${name}!`
