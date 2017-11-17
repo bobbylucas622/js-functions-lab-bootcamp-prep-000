1d1287bf68f6e0762427af62982935a0771faaa1
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-17 20:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // write your code below!
+function happyHolidays() {
+return("Happy holidays!");
+}
+function happyHolidaysTo(you) {
+return("Happy holidays, you!");
+}
+function happyHolidayTo(holiday, name)
+return("Happy ${holiday}, ${name}!")
+}
+function holidayCountdown(noliday,days) {
+ return("It's ${days} days until ${holiday}!");
+}
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
