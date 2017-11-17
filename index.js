73b1faec4157fa1dc321a363d6b77dc078486e87
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-17 21:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function happyHolidaysTo(you) {
 return("Happy holidays, you!");
 }
 function happyHolidayTo(holiday, name)
-return("Happy ${holiday}, ${name}!");
+return(`Happy ${holiday}, ${name}!`);
 }
 function holidayCountdown(holiday,days) {
- return("It's ${days} days until ${holiday}!");
+ return(`It's ${days} days until ${holiday}!`);
 }
 
   
